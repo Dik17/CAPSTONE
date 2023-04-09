9f0a90841098156dc6f634e812fc712f32e295cd
an-anya01 <105367120+an-anya01@users.noreply.github.com>
2023-04-09 22:42:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import React,{useState} from 'react';
+import './App.css';
+import Register from "./components/forms/register";
+
+const App=() =>  {
+  return (
+   <div>
+    <Register/>
+    </div>
+  );
+}
+
+export default App;
